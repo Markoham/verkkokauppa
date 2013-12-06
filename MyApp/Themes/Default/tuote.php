28eb150174baca7f1e828c1d4997c6571c16b80f
Marko <marko.hamppula@metropolia.fi>
2013-12-06 13:45:33 +0200

    Tuote sivu tehty

--- MyApp/Themes/Default/tuote.php
+++ MyApp/Themes/Default/tuote.php
@@ -1,6 +1,10 @@
-<?php
-    $tuote = $framework->getTuote();
-?>
-<h1><?php echo $tuote->getTuotteennimi(); ?></h1>
-<?php echo $tuote->getKuvaus(); ?>
-<?php echo $tuote->getHinta(); ?>
+<div id="productpage">
+    <?php
+        $tuote = $framework->getTuote();
+    ?>
+    <button type="button" class="btn btn-primary" onClick="addTuote(<?php echo $tuote->getId(); ?>)"><span class="glyphicon glyphicon-plus"></span> Lisää koriin</button>
+    <h1><?php echo $tuote->getTuotteennimi(); ?></h1>
+    <h3>Hinta: <?php echo $tuote->getHinta(); ?></h3>
+    <div class="picture"><img src="productimage.php?product=<?php echo $tuote->getId(); ?>" alt="" /></div>
+    <div class="description"><?php echo \Michelf\Markdown::defaultTransform($tuote->getKuvaus()); ?></div>
+</div>
