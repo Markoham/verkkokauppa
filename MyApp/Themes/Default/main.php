71bcff993ef154d169c29edbe9cf701e5751847e
Marko <marko.hamppula@metropolia.fi>
2013-12-06 13:47:02 +0200

    Uusimmat tuotteet etusivulle lisätty

--- MyApp/Themes/Default/main.php
+++ MyApp/Themes/Default/main.php
@@ -1,4 +1,12 @@
 <h1>Tervetuloa Kahvimaailmaan!</h1>
-<p>Tarjouksessa olevat tuotteet...</p>
-<p>Uutuudet...</p>
-<p>Myydyimmät...</p>
+<p>Viimeksi lisätyt tuotteet:</p>
+<div id="recentlyadded">
+<?php
+$tuotteet = $framework->getRecentlyAdded(5);
+foreach($tuotteet as $tuote)
+{
+    echo "<div class=\"product\"><a href=\"?product=" . $tuote->getId() . "\"><img src=\"productimage.php?product=" . $tuote->getId() . "\" alt=\"\"><div>" . $tuote->getTuotteennimi() . "</div></a></div>";
+}
+?>
+</div>
+<div class="clear"></div>
